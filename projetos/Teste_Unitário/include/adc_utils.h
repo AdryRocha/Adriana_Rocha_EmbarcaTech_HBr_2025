1761cc6f156a768eb2e26cfbca8115f8688f4da3
AdryRocha <72994857+AdryRocha@users.noreply.github.com>
2025-05-02 00:35:54 -0300

    Add files via upload

---
+++ projetos/Teste_Unitário/include/adc_utils.h
@@ -0,0 +1,10 @@
+#ifndef ADC_UTILS_H // ADC_UTILS_H
+#define ADC_UTILS_H // Previne inclusão múltipla do arquivo de cabeçalho
+
+#include <stdint.h> // Inclui a biblioteca para tipos de dados inteiros padrão, como uint16_t
+
+float adc_to_celsius(uint16_t adc_val); // Declara a função adc_to_celsius que converte o valor do ADC em temperatura em Celsius
+
+#endif // ADC_UTILS_H
+// Fim do arquivo adc_utils.h
+
